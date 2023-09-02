88f14f6374af01c922be72c7c0196fe5dd41c306
Sofwan Lawal <sofwan@lifebit.ai>
2023-09-02 17:06:33 +0100

    chore: improving usability

--- src/contracts/user.rs
+++ src/contracts/user.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use validator::Validate;
 
-#[derive(Debug, Serialize, Deserialize, Validate)]
+#[derive(Debug, Serialize, Deserialize, Validate, Clone)]
 pub struct CreateUser {
     #[validate(length(min = 3, message = "Name must be greater than 3 chars"))]
     pub name: String,
