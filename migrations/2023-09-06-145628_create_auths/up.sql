596225959fe369e37b58c768c38d8bf2dadca6b6
Sofwan Lawal <sofwan@lifebit.ai>
2023-09-12 18:05:15 +0100

    chore: auths update

--- migrations/2023-09-06-145628_create_auths/up.sql
+++ migrations/2023-09-06-145628_create_auths/up.sql
@@ -1,7 +1,7 @@
 -- Your SQL goes here
 CREATE TABLE IF NOT EXISTS auths(
     id            UUID PRIMARY KEY NOT NULL,
-    user_id       UUID             NOT NULL REFERENCES users(id),
+    user_id       UUID             NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     expires_at    TIMESTAMP        NOT NULL,
     created_at    TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at    TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP
